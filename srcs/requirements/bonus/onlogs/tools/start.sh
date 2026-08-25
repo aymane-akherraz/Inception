60dc0f89225f6807826baa7928792bfcb03694c1
user <aakherra@Deb13.myguest.virtualbox.org>
2026-08-25 19:37:52 +0100

    Modified some files

--- srcs/requirements/bonus/onlogs/tools/start.sh
+++ srcs/requirements/bonus/onlogs/tools/start.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
+export ADMIN_USERNAME="$(grep "^ADMIN_USERNAME=" "/run/secrets/credentials" | cut -d'=' -f2)"
 export ADMIN_PASSWORD="$(grep "^ADMIN_PASSWORD=" "/run/secrets/credentials" | cut -d'=' -f2)"
 
 exec onlogs

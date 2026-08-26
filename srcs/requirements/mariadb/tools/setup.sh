6079377defa0eaced0e4345d3872f863271bfb92
Aymane Akherraz <aymaneakherraz@gmail.com>
2026-08-26 18:18:37 +0100

    Refactord code

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -8,17 +8,14 @@ MARIADB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
 chown -R mysql:mysql /var/lib/mysql
 
-# Initialize MariaDB data directory if necessary
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB..."
     mariadb-install-db --user=mysql
 fi
 
-# Start MariaDB temporarily
 mariadbd --user=mysql &
 pid=$!
 
-# Wait for MariaDB
 until [ -S /run/mysqld/mysqld.sock ]; do
     sleep 1
 done
@@ -41,10 +38,8 @@ $MYSQL -e "FLUSH PRIVILEGES;"
 
 echo "MariaDB: Configuration complete."
 
-# Stop temporary server
 mysqladmin -u root -p"${MARIADB_ROOT_PASSWORD}" shutdown
 
 wait "$pid"
 
-# Start MariaDB as the main container process
 exec mariadbd --user=mysql
